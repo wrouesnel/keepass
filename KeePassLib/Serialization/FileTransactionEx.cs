d47ddbd98f6ec82c09087ab558c5446d5700f2db
Will Rouesnel <w.rouesnel@gmail.com>
2013-07-19 15:56:37 +1000

    Safer file-transactions
     * FileTransactionEx.cs : added a check for the existence of the temporary
    	file before deleting the base file in CommitWriteTransaction()

--- KeePassLib/Serialization/FileTransactionEx.cs
+++ KeePassLib/Serialization/FileTransactionEx.cs
@@ -104,7 +104,14 @@ namespace KeePassLib.Serialization
 			bool bEfsEncrypted = false;
 #endif
 
-			if(IOConnection.FileExists(m_iocBase))
+			// Check that temporary file exists.
+			if (!IOConnection.FileExists(m_iocTemp))
+			{
+				throw new FileNotFoundException("Temporary file disappeared before transaction completed.", 
+				                                m_iocTemp.Path);
+			}
+
+			if (IOConnection.FileExists(m_iocBase))
 			{
 #if (!KeePassLibSD && !KeePassRT)
 				if(m_iocBase.IsLocalFile())
